f55a00dddf3be45f2fcad0ef6bb1bae82bf45171
hyemi1129 <kimhyemi1243@gmail.com>
2024-01-15 12:34:53 +0900

    1st study2

--- Quiz.py
+++ Quiz.py
@@ -1,3 +1,23 @@
+# 변수 사용 퀴즈
+# 변수명 : station / 변수값 : 사당, 신도림, 인천공항 / 출력문장 : XX행 열차가 들어오고 있습니다.
+
+station = "사당"
+print(station + "행 열차가 들어오고 있습니다.")
+station = "신도림"
+print(station + "행 열차가 들어오고 있습니다.")
+station = "인천공항"
+print(station + "행 열차가 들어오고 있습니다.")
+
+
+
+# 랜덤 날짜 뽑기
+
+from random import *
+
+date = randint(4, 28)
+print("오프라인 스터디 모임 날짜는 매월 "+ str(date) + "일로 선정되었습니다.")
+
+
 #비번 만들기
 
 # 예) http://naver.com
