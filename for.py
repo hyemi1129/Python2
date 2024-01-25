3ef4e0df86209471c9fdcf8bb068508749d0c09c
hyemi1129 <kimhyemi1243@gmail.com>
2024-01-25 12:21:27 +0900

    01/25

--- for.py
+++ for.py
@@ -1,4 +1,4 @@
-for i in range(1, 5):
+""" for i in range(1, 5):
     print(i)
 
 for j in range(5):
@@ -6,9 +6,17 @@ for j in range(5):
 
 for i in range(2, 10):
     for j in range(1, 10):
-#        print(f"{i}x{j}={i*j}")
+# print(f"{i}x{j}={i*j}")
         print(i*j)
 
 for i in range(2, 10):
     for j in range(1, 10):
-        print(i, "x", j, "=", i*j)
+        print(i, "x", j, "=", i*j) """
+
+for wating in range (1, 6):
+    print("대기번호 : {0}".format(wating))
+
+star = ["처녀", "사수", "물병"]
+
+for i in star:
+    print("{0}, 커피 준비 다 됨".format(i))
--- if.py
+++ if.py
@@ -1,6 +1,19 @@
-weather = "햇빛"
+weather = input("오늘 날씨는 어때? ")
 
-if weather == "비":
-    print("비가 옵니다.")
+if weather == "비" or weather == "눈":
+    print("우산을 챙겨")
+elif weather == "미세먼지":
+    print("마스크를 챙겨")
 else:
-    print("비가 오지 않습니다.")
+    print("그냥 나가")
+
+
+temp = int(input("기온은 어때? "))
+if 30 <= temp:
+    print("나가지마. 더워")
+elif 10 <= temp and temp <= 30:
+    print("나가도 돼")
+elif 0 <= temp and temp < 10:
+    print("겉옷 ㄱ")
+else:
+    print("나가지마. 추워")
